0e9443990c1b4a5d492247e01d6a8a5534f1289c
gregcoward <g.coward@f5.com>
2020-12-29 12:11:57 -0800

    updated workload scaleset

--- az/appservVmss.tf
+++ az/appservVmss.tf
@@ -8,7 +8,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "backendvmss" {
   location                        = azurerm_resource_group.main.location
   resource_group_name             = azurerm_resource_group.main.name
   sku                             = var.instance_type
-  instances                       = 2
+  instances                       = 1
   admin_username                  = var.uname
   admin_password                  = var.upassword
   disable_password_authentication = false
@@ -66,7 +66,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "backendvmss" {
 
 
 resource "azurerm_monitor_autoscale_setting" "backendvmss" {
-  name                = "myAutoscaleSetting_backend"
+  name                = "myapp-${var.app}"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   target_resource_id  = azurerm_linux_virtual_machine_scale_set.backendvmss.id
@@ -77,55 +77,7 @@ resource "azurerm_monitor_autoscale_setting" "backendvmss" {
     capacity {
       default = 1
       minimum = 1
-      maximum = 10
-    }
-
-    rule {
-      metric_trigger {
-        metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.backendvmss.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "GreaterThan"
-        threshold          = 75
-      }
-
-      scale_action {
-        direction = "Increase"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
-    }
-
-    rule {
-      metric_trigger {
-        metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_linux_virtual_machine_scale_set.backendvmss.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "LessThan"
-        threshold          = 25
-      }
-
-      scale_action {
-        direction = "Decrease"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
-    }
-  }
-
-  notification {
-    email {
-      send_to_subscription_administrator    = true
-      send_to_subscription_co_administrator = true
-      custom_emails                         = ["admin@contoso.com"]
+      maximum = 5
     }
   }
 }
--- az/bigip.tf
+++ az/bigip.tf
@@ -267,7 +267,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "f5vmss" {
   location                        = azurerm_resource_group.main.location
   resource_group_name             = azurerm_resource_group.main.name
   sku                             = var.instance_type
-  instances                       = 2
+  instances                       = 1
   admin_username                  = var.uname
   admin_password                  = var.upassword
   disable_password_authentication = false
