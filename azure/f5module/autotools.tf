2a3aca51d1a56d4cfad000b28551afe69b3cc8be
gregcoward <g.coward@f5.com>
2021-05-26 14:33:21 -0700

    update

--- azure/f5module/autotools.tf
+++ azure/f5module/autotools.tf
@@ -40,14 +40,13 @@ data "template_file" "as3_json" {
   }
 }
 
-data "template_file" "ts_json" {
-  template   = file("${path.module}/ts.json")
-
-  vars = {
-    logStashIP      = "10.2.1.125"
-    splunkIP        = "10.2.1.135"
-    law_id          = var.law_id
-    law_primarykey  = var.law_primarykey 
-    region          = data.azurerm_resource_group.bigiprg.location
-  }
-}
+//data "template_file" "ts_json" {
+//  template   = file("${path.module}/ts.json")
+//  vars = {
+//    logStashIP      = "10.2.1.125"
+//    splunkIP        = "10.2.1.135"
+//    law_id          = var.law_id
+//    law_primarykey  = var.law_primarykey 
+//    region          = data.azurerm_resource_group.bigiprg.location
+//  }
+//}
